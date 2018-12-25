0ce93338817d774944416d682ece8192c0cc5bbd
Mohammad Rajaul Islam <springapidev@gmail.com>
2018-12-26 03:14:43 +0600

    sds update...

--- src/main/java/com/coderbd/onlinebanking/OnlinebankingApplication.java
+++ src/main/java/com/coderbd/onlinebanking/OnlinebankingApplication.java
@@ -1,13 +1,35 @@
 package com.coderbd.onlinebanking;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import javax.faces.webapp.FacesServlet;
 
 @SpringBootApplication
-public class OnlinebankingApplication {
+@ComponentScan(basePackages = "com.coderbd.onlinebanking.*")
+public class OnlinebankingApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(OnlinebankingApplication.class, args);
+    }
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+        FacesServlet servlet = new FacesServlet();
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(servlet, "*.jsf");
+
+        return servletRegistrationBean;
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(OnlinebankingApplication.class);
     }
 
 }
